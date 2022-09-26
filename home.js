933cf0ac6337865eb53a47c23958b05f920d0820
harsha <venkatasriharsha.m17@iiits.in>
2022-09-26 10:43:17 -0500

    Intial commit

---
+++ home.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express()
+var cors = require('cors');
+const port = 3000
+request = require('request')
+const needle = require('needle')
+
+app.use(cors());
+
+const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
+app.get('/', async (req, res) => {
+    if(req.query){
+        const params = {
+            'query': req.query.query + ' lang:en' + ' from:@POTUS',
+            'max_results':90
+        }
+        if(req.query.from){
+            params['start_time']=req.query.from;
+        }
+        if(req.query.to){
+            params['end_time']=req.query.end;
+        }
+
+        const response = await needle('get', endpointUrl, params, {
+            headers: {
+                "User-Agent": "v2RecentSearchJS",
+                "authorization": `Bearer ${'AAAAAAAAAAAAAAAAAAAAAAPPhQEAAAAAaoj4BBhu3l8FBzRDnU1WCCKB6us%3D7qNTh76gutoHPS5ZMl75gmzxjgxYRzu6QWHc5uQvrOMSQ5riJe'}`
+            }
+        })
+    
+        if (response.body) {
+            console.log(response.body)
+            res.jsonp({data:response.body.data})
+        } else {
+            throw new Error('Unsuccessful request');
+        }
+       
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
